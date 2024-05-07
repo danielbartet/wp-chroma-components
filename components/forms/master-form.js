46b1277244e124a9bc896e99db9784235ce032bd
Daniel Bartet <dbartet@progroupx.com>
2024-05-06 23:21:52 -0300

    upgrade webpack and dependencies

--- components/forms/master-form.js
+++ components/forms/master-form.js
@@ -1,5 +1,5 @@
 'use strict'
-import {chromaFormHandler} from  "./signup/form-action.js"
+import chromaFormHandler from  "./signup/form-action.js"
 
 const formProceszr = new chromaFormHandler()
 formProceszr.init()
--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,10 @@
 const path = require('path');
-const webpack = require('webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // Importación corregida
-const autoprefixer = require('autoprefixer');
+const webpack = require('webpack');  // Asegúrate de que esta línea está presente
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
     entry: {
@@ -17,34 +17,28 @@ module.exports = {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist')
     },
-    devServer: {
-        contentBase: './dist',
-        compress: true
-    },
-    watch: true,
-    watchOptions: {
-        ignored: /node_modules/,
-        poll: 3000 // Check for changes every second
-    },
     optimization: {
         minimizer: [
-            new UglifyJsPlugin({
-                cache: true,
-                parallel: true
+            new TerserPlugin({
+                parallel: true,
+                terserOptions: {
+                    ecma: 2016,
+                    compress: true,
+                    format: {
+                        comments: false,
+                    },
+                },
+                extractComments: false,
             }),
-            new OptimizeCssAssetsPlugin({})
+            new CssMinimizerPlugin()
         ]
     },
     plugins: [
-        new CleanWebpackPlugin(), // Se utiliza la nueva importación corregida
-        new MiniCssExtractPlugin({
-            filename: 'cmquiz.css',
-        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({ filename: '[name].css' }),
         new webpack.LoaderOptionsPlugin({
             options: {
-                postcss: [
-                    autoprefixer()
-                ]
+                postcss: [autoprefixer()]
             }
         })
     ],
@@ -55,13 +49,6 @@ module.exports = {
                 exclude: /node_modules/,
                 use: [
                     {
-                        loader: 'eslint-loader',
-                        options: {
-                            fix: true,
-                            cache: true
-                        }
-                    },
-                    {
                         loader: 'babel-loader',
                         options: {
                             presets: ['@babel/preset-env']
@@ -71,11 +58,17 @@ module.exports = {
             },
             {
                 test: /\.(sa|sc|c)ss$/,
-                exclude: /node_modules/,
                 use: [
                     MiniCssExtractPlugin.loader,
                     'css-loader',
-                    'postcss-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [autoprefixer()]
+                            }
+                        }
+                    },
                     'sass-loader'
                 ]
             },
